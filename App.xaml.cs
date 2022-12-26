f73dbdb5006a1b5c884326ef748e8aab3f72344d
fishman123456 <den9152@yandex.ru>
2022-12-27 06:21:56 +0700

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _27_12_2022_WPF_Calculator_Fishman
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
